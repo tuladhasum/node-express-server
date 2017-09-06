eba61d1be9e7bf1c38f454559cd1de6027302388
Sumit Tuladhar <sumittuladhar@gmail.com>
2017-09-05 20:48:38 -0500

    added mongodb connection string in config

--- bookserver/app.js
+++ bookserver/app.js
@@ -2,17 +2,27 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var config = require('./config');//https://stackoverflow.com/questions/22348705/best-way-to-store-db-config-in-node-js-express-app
 
 // connect to mongoose
-mongoose.connect('mongodb://localhost/bookstore',{
+mongoose.connect(config.mongoConnectionString,{
   useMongoClient: true
 },function(error){
   console.log(error);
 });
 var db = mongoose.connection;
 
+console.log(db);
 app.get('/', function(req, res){
-  res.send('hello world from top of the world');
+  res.send('Please use /api/books or /api/genres');
+});
+
+app.get('/api/books', function(req, res){
+  res.send('reached /api/books');
+});
+
+app.get('/api/genres', function(req, res){
+  res.send('reached /api/genres');
 });
 
 
---
+++ bookserver/config-template.js
@@ -0,0 +1,8 @@
+// Rename this file with config.js and install your own mongodb connection string
+
+var config = {
+  mongoConnectionString: ''
+
+};
+
+module.exports = config;
