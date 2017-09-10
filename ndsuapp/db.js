68d7a27f0e5ea96447eb425248e6d97854dec392
Sumit Tuladhar <sumittuladhar@gmail.com>
2017-09-10 18:15:52 -0500

    modified project name and .env.default

--- ndsuapp/db.js
+++ ndsuapp/db.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME 
   // database: 'test'
 });
 
